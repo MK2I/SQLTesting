364ebffca487fb7d8815eb0a13a177b6cdae92b1
Miki <108610162+MK2I@users.noreply.github.com>
2024-09-21 10:47:43 +0200

    Add files via upload
    /*Create a list of participants in Colorado with the columns
    Team, SHIRT_OR_HAT,FIRST_NAME and LAST_NAME
    Organize the list so the participants are sorted by TEAM name
    then by SHIRT or HAT and finally by LAST NAME
    TEAM --->SHIRT or HAT---->LAST NAME ----> ASCENDING else*/

---
+++ Exercise_1.sql
@@ -0,0 +1,12 @@
+/*Create a list of participants in Colorado with the columns
+Team, SHIRT_OR_HAT,FIRST_NAME and LAST_NAME
+Organize the list so the participants are sorted by TEAM name
+then by SHIRT or HAT and finally by LAST NAME
+TEAM --->SHIRT or HAT---->LAST NAME ----> ASCENDING else*/
+
+SELECT team, shirt_or_hat, last_name
+FROM people;
+
+
+
+
